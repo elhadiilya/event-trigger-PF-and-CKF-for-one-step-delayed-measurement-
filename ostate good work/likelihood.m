5ecdb6ad9ed5381a0f8dc642480ac0b24f8e7867
elhadi gasmi <42674874+elhadiilya@users.noreply.github.com>
2023-03-24 14:34:10 +0200

    Delete ostate good work directory

--- ostate good work/likelihood.m
+++
@@ -1,5 +0,0 @@
-function W = likelihood(z,zhat,R)
-    M=size(zhat,2);
-         d=(exp(-0.5*sum((repmat(z,1,M) - zhat).*(inv(R)*(repmat(z,1,M) - zhat)),1)));% 3. Evaluate the likelihood
-         W=((d));
-end
